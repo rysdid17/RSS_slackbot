77ca0c06d890a56614131e9fad52569a568affc5
rysdid17 <ryu23800@gmail.com>
2021-11-22 19:55:07 +0900

    delete unneeded line

--- get_links.py
+++ get_links.py
@@ -1,6 +1,5 @@
 import feedparser
 import requests
-from datetime import datetime
 
 def get_link(url, today, list_link):
     rss = requests.get(url).text
@@ -10,5 +9,4 @@ def get_link(url, today, list_link):
         pubDate = str(entry['updated_parsed'][0]) + str(entry['updated_parsed'][1]) + str(entry['updated_parsed'][2])
         if today == pubDate:
             list_link.append(entry['link'])        
-    return list_link
-today = datetime.now().strftime('%Y%m%d')
+
