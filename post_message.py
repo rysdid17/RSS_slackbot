94efe790f8f54b1629eb05dd8e861a2269c7bc88
rysdid17 <ryu23800@gmail.com>
2021-12-01 23:40:51 +0900

    add blank

--- post_message.py
+++ post_message.py
@@ -2,7 +2,7 @@ import requests
  
 def send_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": text}
+        headers={"Authorization": "Bearer " + token},
+        data={"channel": channel, "text": text}
     )
     print(response)
