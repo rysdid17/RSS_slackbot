b36da917f890864a0525728226fa2b6480ae8f42
rysdid17 <ryu23800@gmail.com>
2022-03-15 00:20:06 +0900

    add feed url_list

--- url_list.py
+++ url_list.py
@@ -1,3 +1,19 @@
-urlList = ['https://techblog.woowahan.com/feed/',
-            'https://tech.kakao.com/feed/',
-          "https://helloworld.kurly.com/feed"]
+urlList = ['https://techblog.woowahan.com/feed/', #우아한 형제들
+            'https://tech.kakao.com/feed/', #카카오
+            'https://helloworld.kurly.com/feed', #마켓컬리
+            'https://meetup.toast.com/rss', #NHN 클라우드
+            'https://engineering.linecorp.com/ko/feed/', #라인
+            'https://tech.socarcorp.kr/feed', #쏘카
+            'https://www.ridicorp.com/story-category/tech-blog/feed/', #리디
+            'https://netflixtechblog.com/feed', #넷플릭스
+            'https://engineering.fb.com/feed/', #페이스북
+            'https://techcommunity.microsoft.com/plugins/custom/microsoft/o365/custom-blog-rss?tid=-865343814338114579&size=25', #MS
+            'https://engineering.linkedin.com/blog.rss.html', #링크드인
+            'https://slack.engineering/feed/', #슬랙
+            'https://technology.riotgames.com/news/feed', #라이엇
+            'https://tech.ebayinc.com/rss', #이베이
+            'https://blog.twitter.com/engineering/en_us/blog.rss', #트위터
+            'https://eng.uber.com/feed/', #우버
+            'https://engineering.atspotify.com/feed/', #스포티파이
+            'https://dropbox.tech/feed' # 드롭박스
+            ]
